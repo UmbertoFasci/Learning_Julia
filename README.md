a0a5a299a74590b33a0d04f513ba3f05632e517e
UmbertoFasci <uj_fasci@dusty.tamiu.edu>
2022-10-20 17:36:50 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learning JULIA
+
+A collection of JULIA code to practice and learn so that i may add this tool to my stats stack.
---
+++ Spatial_Ecology/src/Spatial_Ecology_1.jl
@@ -0,0 +1,6 @@
+using SpatialEcology, Plots, CSV, DataFrames, Statistics
+
+# the object constructors take a wide range of objects, a typical one being a presence-absence matrix as a
+# DataFrame and a 3-column dataframe with coordinates.
+amphdata = CSV.read(joinpath(dirname(pathof(SpatialEcology)), "..", "data", "amph_Europe.csv"));
+amphdata[1:3,1:6]
