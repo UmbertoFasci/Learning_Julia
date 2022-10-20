adbf055965bd1cd3410f84c54dee8a325cf8aad6
UmbertoFasci <uj_fasci@dusty.tamiu.edu>
2022-10-20 17:37:37 -0500

    Initiate assemblage function

--- Spatial_Ecology/src/Spatial_Ecology_1.jl
+++ Spatial_Ecology/src/Spatial_Ecology_1.jl
@@ -4,3 +4,5 @@ using SpatialEcology, Plots, CSV, DataFrames, Statistics
 # DataFrame and a 3-column dataframe with coordinates.
 amphdata = CSV.read(joinpath(dirname(pathof(SpatialEcology)), "..", "data", "amph_Europe.csv"));
 amphdata[1:3,1:6]
+
+amph = Assemblage(amphdata[4:end],amphdata[1:3], sitecolumns = false)
